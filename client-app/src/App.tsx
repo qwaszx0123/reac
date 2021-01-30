fc0838d5b7d6b018ebefab61bf4f90d750a9e384
qwaszx0123 <ocala3526@gmail.com>
2021-01-30 18:55:57 -0500

    29 return hardcode value to display in react

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -4,16 +4,57 @@ import './App.css';
 import axios from 'axios';
 
 class App extends Component {
+  // 00:00:18.170 ~ 00:00:20.960  And to do this as I mentioned each we act.
+  // 00:00:20.960 ~ 00:00:26.470  Class component can use component states and to do this.
+  // 00:00:26.470 ~ 00:00:36.050  We can declare states objects inside the class and we can say states equals and open some curly brackets.
   state = {
     values: []
   };
 
+  // 00:02:04.050 ~ 00:02:09.560  But just to keep things as straightforward and simple as possible what we're doing here is when our
+  // 00:02:09.560 ~ 00:02:20.050  component first loads the state is going to be an empty array inside this values property and after
+
+
+
+  // 00:04:10.250 ~ 00:04:17.080  But very briefly what we're looking at here is not hasty AML but it is actually javascript and it's
+  // 00:04:17.080 ~ 00:04:24.790  javascript styled in such a way as it makes it look and feel like we're writing aged CML but it is actually
+  // 00:04:24.790 ~ 00:04:30.310  javascript and it gives us a great deal of advantage here because we can write our components as though
+  // 00:04:30.310 ~ 00:04:37.630  we're just making a static hasty AML page but we've got all of the functionality available hands to
+  // 00:04:37.630 ~ 00:04:44.560  add whatever javascript functionality we want inside here and like I say we'll get deeper into what
+
+
+  //change from function compont to class component  need render (){}
+
+
+    // 00:00:36.210 ~ 00:00:41.940  And then for now we're just going to stick with our simple little values and our values is going to
+  // 00:00:41.940 ~ 00:00:51.400  be an array that we store inside our states object and what we'll also do is use a component the Mount
+  // 00:00:52.110 ~ 00:00:59.430  method reacts lifecycle method which is called immediately after a component is mounted.
+  // 00:00:59.430 ~ 00:01:04.950  And when we set state here we trigger a rebrand of our component.
+  // 00:01:05.280 ~ 00:01:12.120  And then our updated status is displayed on our page and we'll be able to see this in action.
+
+
+  // 00:00:59.430 ~ 00:01:04.950  And when we set state here we trigger a rebrand of our component.
+
+  // componentDidMount() {
+  //   axios.get('http://localhost:5000/api/values').then(response => {
+  //     this.setState({
+  //       values: response.data
+  //     });
+  //   });
+  // }
+
   componentDidMount() {
-    axios.get('http://localhost:5000/api/values').then(response => {
-      this.setState({
-        values: response.data
-      });
-    });
+    //  00:01:18.610 ~ 00:01:27.250  a we act component class method called this dot set state so that we can modify what's going on inside
+    this.setState({
+      values:[{
+        Id : 1,
+        Name : "Value 101"
+    },
+    {
+        Id :2,
+        Name : "Value 102"
+    }]
+    })
   }
 
   render() {
@@ -28,6 +69,15 @@ class App extends Component {
             <List.Item key={value.id}>{value.name}</List.Item>
           ))}
         </List>
+{/* use map to loop */}
+        <ul>
+          {this.state.values.map((value : any)=>(
+
+            <li>{value.Name}</li>
+          ))}
+
+
+        </ul>
       </div>
     );
   }
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -6,6 +6,10 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+// 00:02:49.640 ~ 00:02:56.420  Dom has a single method and react on calls a render method and what this does it gets the reference
+// 00:02:56.420 ~ 00:03:06.380  to the root element or I div with the idea of root and then inside this particular div it loads are
+// 00:03:06.410 ~ 00:03:13.340  apt components and it's the contents of this component that we're seeing outputs into our browser page
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
